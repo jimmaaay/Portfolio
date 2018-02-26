2e043c9148061713185b59b5df3bd716b01e3274
jimmaaay <thisguy@jimmythompson.me>
2018-02-26 13:58:41 +0000

    Bug fix & Added json file generation for list of portfolio items

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ My portfolio site built using [hugo](https://gohugo.io/)
 * `npm run dev` - Starts a browserSync server and will reload on content/asset changes
 
 * `npm run build` - Builds the site ready for production and places files in the `dist` directory
+
+
+### Notes
+
+#### JSON file generation
+https://discourse.gohugo.io/t/generate-json-from-data-files/8083
--- src/js/header.js
+++ src/js/header.js
@@ -1,13 +1,15 @@
 import { supportsPassive } from './helpers';
 
 const header = document.querySelector('.header');
+const headerScroll = () => {
+  if (window.pageYOffset === 0) {
+    header.classList.remove('header--scrolled');
+  } else {
+    header.classList.add('header--scrolled');
+  }
+};
 
 if (header != null) {
-  window.addEventListener('scroll', () => {
-    if (window.pageYOffset === 0) {
-      header.classList.remove('header--scrolled');
-    } else {
-      header.classList.add('header--scrolled');
-    }
-  }, supportsPassive ? { passive: true } : false);
+  headerScroll();
+  window.addEventListener('scroll', headerScroll, supportsPassive ? { passive: true } : false);
 }
