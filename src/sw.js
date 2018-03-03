7b10dfdfdee9381bee876cbec07095d5191205b8
James Thompson <thisguy@jimmythompson.me>
2018-03-03 19:42:42 +0000

    WIP service worker

--- src/sw.js
+++ src/sw.js
@@ -1,1 +1,22 @@
-console.log('wurking');
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches
+      .open(variables.cacheName)
+      // Add the initial files to the sw cache
+      .then(cache => cache.addAll(variables.files))
+  );
+});
+
+// Remove any old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== variables.cacheName) return caches.delete(key);
+        return true;
+      })
+    ))
+  );
+});
--- tasks/serviceWorker.js
+++ tasks/serviceWorker.js
@@ -11,11 +11,13 @@ const serviceWorker = async function serviceWorker(done) {
     // Add a blank service worker when running the site on localhost
     await writeFileAsync('./dist/sw.js', '');
   } else {
+    // Files to get
     const patterns = [
       './dist/index.html',
       './dist/js/**/*.js',
       './dist/styles/**/*.css',
       './dist/img/svgSprites/**/*.svg',
+      './dist/other/fonts/**/*.woff2',
     ];
     const fileNames = await globHelper(patterns);
     const newFileNames = fileNames.map(fileName => fileName.replace('./dist', ''));
@@ -23,6 +25,7 @@ const serviceWorker = async function serviceWorker(done) {
 
     for (const fileName of fileNames) {
       const fileContents = await readFileAsync(fileName, 'utf-8');
+      // get a hash from the contents of the files that will be required by the SW
       hash.update(fileContents);
     }
 
@@ -32,6 +35,7 @@ const serviceWorker = async function serviceWorker(done) {
       files: newFileNames,
     });
 
+    // get the service worker template
     const swTemlpate = await readFileAsync('./src/sw.js');
 
     await writeFileAsync('./dist/sw.js', `
