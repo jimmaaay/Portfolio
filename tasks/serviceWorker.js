fd75b71bc35ad8e49779b771dc11099549b4cc68
jimmaaay <thisguy@jimmythompson.me>
2018-03-12 14:01:29 +0000

    WIP service worker

--- tasks/serviceWorker.js
+++ tasks/serviceWorker.js
@@ -13,7 +13,8 @@ const serviceWorker = async function serviceWorker(done) {
   } else {
     // Files to get
     const patterns = [
-      './dist/index.html',
+      './dist/partials/index.html',
+      './dist/template/index.html',
       './dist/js/**/*.js',
       './dist/styles/**/*.css',
       './dist/img/svgSprites/**/*.svg',
@@ -21,14 +22,12 @@ const serviceWorker = async function serviceWorker(done) {
       './dist/portfolio/index.json',
     ];
     const fileNames = await globHelper(patterns);
-    const newFileNames = fileNames.map(fileName => fileName.replace('./dist', ''));
+    const newFileNames = fileNames.map(fileName => fileName.replace('./dist', '').replace('index.html', ''));
     const hash = crypto.createHash('md5');
 
-    for (const fileName of fileNames) {
-      const fileContents = await readFileAsync(fileName, 'utf-8');
-      // get a hash from the contents of the files that will be required by the SW
-      hash.update(fileContents);
-    }
+    fileNames.forEach(fileName => {
+      hash.update(fileName);
+    });
 
     const cacheName = hash.digest('hex');
     const variables = JSON.stringify({
